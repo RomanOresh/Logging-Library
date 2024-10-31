68772291e414cb01b9051d5f75482c4ed54110eb
Roman <romanoreshchenko@gmail.com>
2024-10-31 15:33:12 +0100

    Deleted throws Exception in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
        FileLogger logger = new FileLogger();
        logger.info("Started application");
        logger.debug("Calling method X");
